f052194e1a5b6baad5b2ec1352cd940465afe0ab
서울민트초코 <minseong130502@gmail.com>
2026-07-24 23:36:16 +0900

    header 수정

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,16 @@
 import './style.scss';
+const brushColor = document.getElementById('brushColor') as HTMLInputElement;
+const brushColorSpan = document.getElementById('brushColorValue') as HTMLSpanElement;
+brushColor.addEventListener('input', (event) => {
+    const target = event.target as HTMLInputElement;
+    const selectedColor = target.value;
+    brushColorSpan.textContent = selectedColor.toUpperCase();
+});
+
+
+const brushSize = document.getElementById('brushSize') as HTMLInputElement;
+const brushSizeValue = document.getElementById('brushSizeValue') as HTMLSpanElement;
+brushSize.addEventListener('input', (e) => {
+    const target = e.target as HTMLInputElement;
+    brushSizeValue.textContent = target.value;
+});
