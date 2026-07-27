7dea2b5486b491cd8e4f53c2a4239e691c39f692
서울민트초코 <minseong130502@gmail.com>
2026-07-27 09:42:38 +0900

    feat: enhance SCSS color adjustments; refactor canvas initialization in main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,8 @@
 import './style.scss';
 
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
 const brushColor = document.getElementById('brushColor') as HTMLInputElement;
 const brushColorSpan = document.getElementById('brushColorValue') as HTMLSpanElement;
 brushColor.addEventListener('input', (event) => {
@@ -16,8 +19,12 @@ brushSize.addEventListener('input', (e) => {
 });
 
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const paintToggle = document.getElementById('paintToggle') as HTMLInputElement;
+const toggleText = document.querySelector('.toggle_text') as HTMLSpanElement;
+const canvasContainer = document.querySelector('.canvas_container') as HTMLDivElement;
+
+
+
 let isDrawing: boolean = false;
 canvas.width = 3200;
 canvas.height = 2400;
