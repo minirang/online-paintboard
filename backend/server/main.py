d7d25063a8b63d321cff98dc911e19fb9fcbfd46
서울민트초코 <minseong130502@gmail.com>
2026-08-02 12:26:38 +0900

    CORS 미들웨어 추가

--- backend/server/main.py
+++ backend/server/main.py
@@ -4,6 +4,7 @@ import psycopg2
 from pathlib import Path
 from dotenv import load_dotenv
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 
 env_path = Path(__file__).parent / ".env"
 print("Loading env:", env_path)
@@ -13,6 +14,7 @@ conn = psycopg2.connect(DB_URL)
 print("Connected to the database.")
 active_connections = []
 app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 
 @app.get("/")
