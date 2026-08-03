1fd8cfbc54900bb33fd5ff40ecc00b2f061cba48
서울민트초코 <minseong130502@gmail.com>
2026-08-03 21:20:15 +0900

    Increase canvas dimensions to 12800x9600

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,8 @@ import './style.scss';
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const canvasContainer = document.querySelector('.canvas_container') as HTMLDivElement;
-canvas.width = 6400;
-canvas.height = 4800;
+canvas.width = 12800;
+canvas.height = 9600;
 const worker = new Worker(new URL('./paint.worker.ts', import.meta.url), { type: 'module' });
 const offscreen = canvas.transferControlToOffscreen();
 const wsProtocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
