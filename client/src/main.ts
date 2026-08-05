855f0cf20b690bab9afd68b67db3c2d8da6c1d3d
서울민트초코 <minseong130502@gmail.com>
2026-08-05 09:52:03 +0900

    캔버스 사이즈를 늘림

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,8 @@ import './style.scss';
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const canvasContainer = document.querySelector('.canvas_container') as HTMLDivElement;
-canvas.width = 6400;
-canvas.height = 4800;
+canvas.width = 7200;
+canvas.height = 5400;
 const worker = new Worker(new URL('./paint.worker.ts', import.meta.url), { type: 'module' });
 const offscreen = canvas.transferControlToOffscreen();
 
