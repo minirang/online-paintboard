f6e10e3ad0ffddcb440c5b555328201e3d060bda
서울민트초코 <minseong130502@gmail.com>
2026-09-13 20:16:23 +0900

    eslint 설정 업데이트 및 globals 패키지 추가

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,18 +1,33 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import globals from "globals";
 
 export default tseslint.config(
-  {
-    ignores: ["dist/**", "build/**", "node_modules/**"],
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      "semi": ["error", "always"],
-      "@typescript-eslint/no-explicit-any": "warn",
-      "no-console": "off",
-      "@typescript-eslint/no-unused-vars": "warn"
+    {
+        ignores: ["dist/**", "build/**", "node_modules/**"],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            }
+        },
+        rules: {
+            "semi": ["error", "always"],
+            "@typescript-eslint/no-explicit-any": "warn",
+            "no-console": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "no-case-declarations": "warn"
+        }
+    },
+    {
+        files: ["**/sw.js"],
+        languageOptions: {
+            globals: {
+                ...globals.serviceworker
+            }
+        }
     }
-  }
 );
